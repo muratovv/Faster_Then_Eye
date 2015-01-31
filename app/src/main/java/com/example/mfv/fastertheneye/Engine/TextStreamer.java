e8c610f8323a59457463f88fb0d6dd86b06cf748
muratov <muratovfedor@yandex.ru>
2015-01-31 11:22:35 +0300

    small update textstreammer

--- app/src/main/java/com/example/mfv/fastertheneye/Engine/TextStreamer.java
+++ app/src/main/java/com/example/mfv/fastertheneye/Engine/TextStreamer.java
@@ -3,6 +3,7 @@ package com.example.mfv.fastertheneye.Engine;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.*;
 
 /**
  * TextStreamer - организация потококового ввывода из переданного файла.
@@ -12,9 +13,7 @@ public class TextStreamer
 	public TextStreamer(String text)
 	{
 		ArrayList<String> rowFrames = new ArrayList<>(Arrays.asList(text.split("[\b]")));
-		
 	}
-
 
 	public Box next()
 	{
@@ -51,39 +50,48 @@ public class TextStreamer
 	private Box makeSeparation(String frame)
 	{
 		Box resultBox = new Box();
-		String forSeparation = frame;
-		if(forSeparation.length() == 1)
+		Matcher matcher = pattern.matcher(frame);
+		if(matcher.find())
 		{
-			resultBox.pivot = forSeparation;
+			String forSeparation = matcher.group(1);
+			if(forSeparation.length() == 1)
+			{
+				resultBox.pivot = forSeparation;
+			}
+			if(2 <= forSeparation.length() && forSeparation.length() <= 3)
+			{
+				resultBox.leftPart = forSeparation.substring(0, 1);
+				resultBox.pivot = forSeparation.substring(1, 2);
+				resultBox.rightPart = forSeparation.substring(2);
+			}
+			if(4 <= forSeparation.length() && forSeparation.length() <= 6)
+			{
+				resultBox.leftPart = forSeparation.substring(0, 1);
+				resultBox.pivot = forSeparation.substring(1, 2);
+				resultBox.rightPart = forSeparation.substring(2);
+			}
+			if(6 <= forSeparation.length() && forSeparation.length() <= 9)
+			{
+				resultBox.leftPart = forSeparation.substring(0, 2);
+				resultBox.pivot = forSeparation.substring(2, 3);
+				resultBox.rightPart = forSeparation.substring(3);
+			}
+			if(10 <= forSeparation.length() && forSeparation.length() <= 14)
+			{
+				resultBox.leftPart = forSeparation.substring(0, 3);
+				resultBox.pivot = forSeparation.substring(3, 4);
+				resultBox.rightPart = forSeparation.substring(4);
+				assert (resultBox.leftPart + resultBox.pivot + resultBox.rightPart).equals(forSeparation);
+			}
 		}
-		if(2 <= forSeparation.length() && forSeparation.length() <= 3)
+		else
 		{
-			resultBox.leftPart = forSeparation.substring(0, 1);
-			resultBox.pivot = forSeparation.substring(1, 2);
-			resultBox.rightPart = forSeparation.substring(2);
-		}
-		if(4 <= forSeparation.length() && forSeparation.length() <= 6)
-		{
-			resultBox.leftPart = forSeparation.substring(0, 1);
-			resultBox.pivot = forSeparation.substring(1,2);
-			resultBox.rightPart = forSeparation.substring(2);
-		}
-		if(6 <= forSeparation.length() && forSeparation.length() <= 9)
-		{
-			resultBox.leftPart = forSeparation.substring(0, 2);
-			resultBox.pivot = forSeparation.substring(2,3);
-			resultBox.rightPart = forSeparation.substring(3);
-		}
-		if(10 <= forSeparation.length() && forSeparation.length() <= 14)
-		{
-			resultBox.leftPart = forSeparation.substring(0, 3);
-			resultBox.pivot = forSeparation.substring(3,4);
-			resultBox.rightPart = forSeparation.substring(4);
-			assert (resultBox.leftPart + resultBox.pivot + resultBox.rightPart).equals(forSeparation);
+			resultBox.leftPart = frame;
 		}
 		return resultBox;
 	}
 
 	private int currentIndex = 0;
 	private String[] frames;
+	private Pattern pattern = Pattern.compile("\\b\\w+\\b");
 }
